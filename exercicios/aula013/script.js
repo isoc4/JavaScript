6fcfbc52845a4bd81f3544b6f75b27995be571e0
Isis Maria <isocaisismaria@gmail.com>
2024-05-17 19:47:40 -0300

    aula013 e 014

---
+++ exercicios/aula013/script.js
@@ -0,0 +1,41 @@
+//2 melhores soluções (Forma eficiente de fazer um loop)
+
+var cont = 1
+
+while (cont <= 10){
+    console.log(`Hello World! (Passo ${cont})`)
+    cont++ // c = c + 1
+}//Solução mais usada nos programas.
+
+/*
+do{
+    console.log(`Hello World! (Passo ${cont})`)
+    cont++
+} while (cont <= 10)
+*/
+
+
+
+//Pior solução (Forma não eficiente de fazer um loop)
+
+/*
+console.log('Hello World! (Passo 1)')
+console.log('Hello World! (Passo 2)')
+console.log('Hello World! (Passo 3)')
+console.log('Hello World! (Passo 4)')
+console.log('Hello World! (Passo 5)')
+console.log('Hello World! (Passo 6)')
+console.log('Hello World! (Passo 7)')
+console.log('Hello World! (Passo 8)')
+console.log('Hello World! (Passo 9)')
+console.log('Hello World! (Passo 10)')
+*/
+
+
+
+/*
+Já dizia o mestre guanabara para seus gafanhotos: 
+
+"Se você quer ser programador use muito mais a cachola do que a força bruta."
+
+*/
---
+++ exercicios/aula014/script.js
@@ -0,0 +1,5 @@
+console.log('Vai começar!')
+for (var n = 1; n <= 5;n++){
+    console.log(n)
+}
+console.log('Acabou!!')
