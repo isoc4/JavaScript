8ed8a6af09fa48d663f379f9867e13361be1d3d0
Isis Maria <isocaisismaria@gmail.com>
2024-06-08 10:55:21 -0300

    p014: calculando fatorial

--- praticando/p014/script.js
+++ praticando/p014/script.js
@@ -1,14 +1,29 @@
-function fatorial(){
-    var numero = document.getElementById('inumero')
-    var resul = document.getElementById('res')
+function calcularFatorial(){
+    let numero = document.getElementById('inumero')
+    let resul = document.getElementById('res')
+    resul.innerHTML = ''
 
     if (numero.value.length == 0){
         alert('[ERRO] Digite um número.')
-    } else {
-        var n = Number(numero.value)
+    } else if (Number(numero.value) >= 1 && Number(numero.value) <= 21){
+        let contador = Number(numero.value)
+        let fatorial = 1
 
-        for (var contador = 1; contador <= n; contador--){
-            resul.innerHTML += `${n} X ${contador} = ${n * contador}`
+        /*
+            enquanto(o contador(que vai ser o número colocado na let numero) for maior que 1){
+                resul.noHTML adicionar(+=) `${contador} X `
+                fatorial = ele mesmo x o contador
+                contador - 1
+            }
+        */
+        while(contador > 1){
+            resul.innerHTML += `${contador} X `
+            fatorial *=
+            contador--
         }
+
+        resul.innerHTML += `1 = ${fatorial.toLocaleString('PT-BR')}`//toLocaleString('PT-BR') define o numero em mil/milhões...
+    } else {
+        alert('[ERRO] Digite um número entre 1 e 21.')
     }
 }
