159cb4f858a0f33d64672e57dd8b4b992975745a
Isis Maria <isocaisismaria@gmail.com>
2024-06-08 17:03:40 -0300

    wip

---
+++ praticando/outras práticas/calculadora/script.js
@@ -0,0 +1,12 @@
+let resul = document.getElementById('resh2')
+
+function digitarZero(){resul.innerHTML += 0}
+function digitarUm(){resul.innerHTML += 1}
+function digitarDois(){resul.innerHTML += 2}
+function digitarTres(){resul.innerHTML += 3}
+function digitarQuatro(){resul.innerHTML += 4}
+function digitarCinco(){resul.innerHTML += 5}
+function digitarSeis(){resul.innerHTML += 6}
+function digitarSete(){resul.innerHTML += 7}
+function digitarOito(){resul.innerHTML += 8}
+function digitarNove(){resul.innerHTML += 9}
