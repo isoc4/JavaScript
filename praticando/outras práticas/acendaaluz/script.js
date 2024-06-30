66a1140c4dee00531fc34af5999f73030f2dca95
Isis Maria <isocaisismaria@gmail.com>
2024-06-30 10:07:20 -0300

    wip

---
+++ praticando/outras práticas/acendaaluz/script.js
@@ -0,0 +1,13 @@
+let lampada = document.getElementById('lampada')
+let carga = document.getElementById('carga')
+let bateria = document.getElementById('bateria')
+
+carga = true
+bateria = true
+
+let botaoCarga = document.getElementById('bc')
+let botaoBateria = document.getAnimations('bb')
+
+function estaConectado(){
+    alert('CERTO')
+}
